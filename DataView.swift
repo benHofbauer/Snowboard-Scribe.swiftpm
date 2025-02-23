ecadcc5218649dc6402ab4d24d6d730042b46f89
Ben Hofbauer <bph36@case.edu>
2025-02-23 04:33:02 -0500

    added charts

--- DataView.swift
+++ DataView.swift
@@ -6,6 +6,14 @@
 
 import SwiftUI
 import SwiftData
+import Charts
+
+struct trailDifficulty: Identifiable, Hashable {
+    var id = UUID()
+    var rating: Rating
+    var name:String
+    var count:Int
+}
 
 func getDate(date: Date) -> String {
     let dateFormatter = DateFormatter()
@@ -40,6 +48,30 @@ func getUniqueDates(trails:[Trail]) -> ([String:[Trail]], [String]) {
     }
     
     return (newDict, keys)
+}
+
+@available(iOS 17, *)
+func difficultySplits(trails:[Trail]) -> [trailDifficulty] {
+    var splits = [0, 0, 0, 0]
+    
+    for trail in trails {
+        if trail.rating == Rating.green {
+            splits[0] += 1
+        } else if trail.rating == Rating.blue {
+            splits[1] += 1
+        } else if trail.rating == Rating.black {
+            splits[2] += 1
+        } else {
+            splits[3] += 1
+        }
+    }
+    let green = trailDifficulty(rating: .green, name: "Green", count: splits[0])
+    let blue = trailDifficulty(rating: .blue, name: "Blue", count: splits[1])
+    let black = trailDifficulty(rating: .black, name: "Black", count: splits[2])
+    let dblack = trailDifficulty(rating: .double, name: "Double Black", count: splits[3])
+    var out = [green, blue, black, dblack]
+    out = out.sorted { $0.count > $1.count }
+    return out
 }
 
 struct Symbol: View {
@@ -82,8 +114,44 @@ struct DataView: View {
 
     var body: some View {
         let (uniqueDates, days) = getUniqueDates(trails: trails)
+        let splits = difficultySplits(trails: trails)
         NavigationStack {
             List {
+                Section {
+                    Chart(splits, id: \.self) { split in
+                        SectorMark(
+                            angle: .value("Number of runs", split.count),
+                            innerRadius: .ratio(0.618),
+                            angularInset: 1.5
+                        )
+                        .cornerRadius(5)
+                        .foregroundStyle(by: .value("Rating", split.name))
+                    }
+                    .frame(width: 325, height: 325)
+                    .chartForegroundStyleScale([
+                        "Green" : .green,
+                        "Blue" : .blue,
+                        "Black" : .black,
+                        "Double Black" : .gray
+                    ])
+                    .chartBackground { chartProxy in
+                      GeometryReader { geometry in
+                        let frame = geometry[chartProxy.plotAreaFrame]
+                        VStack {
+                            ForEach(splits, id: \.self) { split in
+                                HStack {
+                                    Text("\(split.count)")
+                                    Symbol(rating: split.rating, paddingDimension: CGFloat(0), dimension: CGFloat(15))
+                                }
+                            }
+                        }
+                        .position(x: frame.midX, y: frame.midY)
+                      }
+                    }
+                    .chartLegend(alignment: .bottom, spacing: CGFloat(25))
+                    .padding()
+                }
+                
                 Button("Log Ride") {
                     newScreenShowing.toggle()
                 }
